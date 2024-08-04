db73d2514b44517cefc4f9ab37e8ce67ab9ef35f
boesi <you@example.com>
2024-08-04 20:28:55 +0200

    take headline from config

--- generator/app/files.py
+++ generator/app/files.py
@@ -3,11 +3,27 @@ from bs4 import BeautifulSoup as bs
 import os
 from pathlib import Path
 import sys
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from strictyaml import load, Map, Str
+
+
+def getConfig(path):
+    schema = Map({'headline': Str()})
+    return load(path.joinpath('generator-config.yaml').read_text(encoding='utf8'), schema)
 
 path_base = Path(sys.argv[1] if len(sys.argv) > 1 else '.')
 path_output = path_base.joinpath('static/')
 
-html = path_base.joinpath('src/index.html').open()
+config = getConfig(path_base)
+
+env = Environment(
+        loader = FileSystemLoader(path_base.joinpath("src")),
+        autoescape = select_autoescape()
+)
+template = env.get_template("index.html")
+
+html = template.render(headline=config['headline'])
+# html = path_base.joinpath('src/index.html').open()
 soup = bs(html, 'html.parser')
 html_links = soup.find(class_="cb-links")
 
